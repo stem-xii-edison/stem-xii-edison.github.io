75e0fbee9276e009061c1201b98928754fa41fdb
stem-xii-edison <illuflins69@gmail.com>
2026-06-23 22:52:02 +0800

    Add files via upload

---
+++ stem_xii_edison.js
@@ -0,0 +1,394 @@
+const COLORS = [
+  '#2a2a2a', '#333333', '#1e1e1e', '#3a3a3a', '#242424',
+  '#2e2e2e', '#303030', '#1a1a1a', '#383838', '#282828'
+];
+
+const BATCHES = {
+
+  '2026-2027': [
+    { name: 'Student 1 lol', image: '', height: '', weight: '', birthday: '', role: 'Class President', color: COLORS[0], badges: ['', ''], bio: 'Pleaceholder bio', achievements: [{ title: '', desc: '' },], interests: [''] },
+  ],
+
+  '2025-2026': [
+    //Boys
+    { name: 'Kyle Andre L. Alili', image: 'student_avatars/HKMlPZebcAAFMDb_cropped.webp', height: '', weight: '', birthday: 'September 22, 2008', role: 'Student', color: COLORS[0], 
+      badges: ['3rd Quarter With Honor Student', '4th Quarter With Honor Student'], 
+      bio: 'Obsessed with fictional characters', 
+      achievements: [
+        { title: 'DisFOT 2024 Technolympics Technical Drafting 1st Place', desc: 'District Festival of Talents 2024' }, 
+        { title: 'DFOT 2024 Technolympics Technical Drafting 2nd Place', desc: 'Division Festival of Talents 2024' }, 
+        { title: 'DisFOT 2025 Technolympics Technical Drafting 1st Place', desc: 'District Festival of Talents 2025' }, 
+        { title: 'DFOT 2025 Technolympics Technical Drafting 3rd Place', desc: 'Division Festival of Talents 2025' }, 
+        { title: 'DSPC 2025 Tv Broadcasting 1st Place', desc: 'Division School\'s Press Conference 2025' }], 
+      interests: ['Gaming', 'Reading', 'Music'] },
+    { name: 'Xianvee C. Almacen', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Russelle Cris E. Araniego', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Meshach Caleb A. Balubayan', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Jared A. Boladola', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Jossiah Russel A. Cabarrubias', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Piolo B. Calisagan', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Francis G. Crausos', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Ken Charles J. Dañosos', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Zadrack Jorey C. Labarinto', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Brando C. Marquez', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Zonyd Rae T. Pasonilao', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Harry Benedict I. Ruelan', image: 'student_avatars/', height: '', weight: '', birthday: 'September 22, 2008', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Aaron Rave P. Salazar', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Jipher S. San Juan', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Wincer Cris M. Tabamo', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Prince Jeswel P. Tale', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    //Girls
+    { name: 'Princess Anne Adlawan', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Denice Veane R. Agabon', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Princess Luphea D. Alisna', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Queencybel L. Ambrocio', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Xania Viel P. Ampo', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Xenia Tina Marie E. Araniego', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Junessa Pearl M. Baguio', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Chad Trexie Kate C. Barago', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Sheenah Mariz P. Batoy', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Kristine Joy F. Colong', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Zendy Loraine A. Combista', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Mary Rose P. Erosido', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Maebelle Joy T. Flores', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Rieze Divine C. Galvez', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Razel J. Gane', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Bernadette Jane R. Jandayan', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Ella Mae D. Mangulare', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Abegail Shane R. Montejo', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Glendie Mitz S. Orbino', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Eah P. Paquera', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Hannah C. Polongasa', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Kristine Joy R. Ruelo', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Debbie Mae M. Sante', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+    { name: 'Eliana C. Soroño', image: 'student_avatars/', height: '', weight: '', birthday: 'N/A', role: '', color: COLORS[0], 
+      badges: ['', ''], 
+      bio: '', 
+      achievements: [
+        { title: '', desc: '' }], 
+      interests: [''] },
+  ],
+};
+
+const AWARDS = [
+  { 
+    image: 'awards_images/727446514_122177409572904914_8389721779697111850_n.webp', 
+    title: 'Award Title', 
+    desc: 'Short description about the award', 
+    date: 'January 00, 2000' 
+  },
+];
+
+let activeBatch = Object.keys(BATCHES)[0];
+
+function initBatchTabs() {
+  const bar = document.getElementById('batch-bar');
+  bar.innerHTML = '';
+  Object.keys(BATCHES).forEach(sy => {
+    const btn = document.createElement('button');
+    btn.className = 'batch-tab' + (sy === activeBatch ? ' active' : '');
+    btn.innerHTML = `SY ${sy.replace('-', '–')} <span class="batch-count">${BATCHES[sy].length}</span>`;
+    btn.addEventListener('click', () => {
+      activeBatch = sy;
+      document.querySelectorAll('.batch-tab').forEach(t => t.classList.remove('active'));
+      btn.classList.add('active');
+      document.getElementById('student-search').value = '';
+      renderGrid(BATCHES[activeBatch]);
+    });
+    bar.appendChild(btn);
+  });
+}
+
+function renderGrid(list) {
+  const grid = document.getElementById('students-grid');
+  grid.innerHTML = '';
+  if (!list || list.length === 0) {
+    grid.innerHTML = '<div class="batch-empty"><strong>No Students Yet</strong>Add entries to this batch in the BATCHES data object.</div>';
+    return;
+  }
+  list.forEach((s, i) => {
+    const card = document.createElement('div');
+    card.className = 'student-card';
+    card.style.animationDelay = `${Math.min(i, 8) * 0.04}s`;
+    card.innerHTML = `
+        <div class="avatar" style="background:${s.color};border:1px solid rgba(255,255,255,0.1)">${s.image ? `<img src="${s.image}" alt="${s.name}">` : s.name.split(' ').map(n => n[0]).join('')}</div>
+        <h3>${s.name}</h3>
+        <p class="role">${s.role}</p>
+        <div class="badge-row">${s.badges.map(b => `<span class="badge">${b}</span>`).join('')}</div>
+      `;
+    card.addEventListener('click', () => openProfile(s));
+    grid.appendChild(card);
+  });
+}
+
+function filterStudents(val) {
+  const q = val.toLowerCase();
+  renderGrid(BATCHES[activeBatch].filter(s =>
+    s.name.toLowerCase().includes(q) || s.role.toLowerCase().includes(q)
+  ));
+}
+
+function initGrid() {
+  initBatchTabs();
+  renderGrid(BATCHES[activeBatch]);
+}
+
+function renderAwards() {
+  const grid = document.getElementById('awards-grid');
+  grid.innerHTML = AWARDS.map((a, i) => `
+      <div class="award-card" style="animation-delay:${Math.min(i, 8) * 0.04}s">
+        <div class="award-image">
+          ${a.image ? `<img src="${a.image}" alt="${a.title}">` : `<span class="award-image-placeholder">No Image</span>`}
+        </div>
+        <div class="award-body">
+          <p class="award-date">${a.date}</p>
+          <h3>${a.title}</h3>
+          <p class="award-desc">${a.desc}</p>
+        </div>
+      </div>
+    `).join('');
+}
+
+function openProfile(s) {
+  const avatar = document.getElementById('p-avatar');
+  avatar.innerHTML = s.image ? `<img src="${s.image}" alt="${s.name}">` : s.name.split(' ').map(n => n[0]).join('');
+  avatar.style.background = s.color;
+  document.getElementById('p-name').textContent = s.name;
+  document.getElementById('p-role').textContent = s.role;
+  document.getElementById('p-badges').innerHTML = s.badges.map(b => `<span class="badge">${b}</span>`).join('');
+  document.getElementById('p-bio').textContent = s.bio;
+  document.getElementById('p-heightweight').textContent = s.height && s.weight ? `${s.height}/${s.weight}` : '-cm/-kg';
+  document.getElementById('p-birthday').textContent = s.birthday || '-';
+  document.getElementById('p-achievements').innerHTML = s.achievements.map((a, i) => `
+      <li><div class="ach-icon">${i + 1}</div><div class="ach-text"><strong>${a.title}</strong><span>${a.desc}</span></div></li>
+    `).join('');
+  document.getElementById('p-interests').innerHTML = s.interests.map(i => `<span class="interest-tag">${i}</span>`).join('');
+  document.getElementById('profile-overlay').classList.add('open');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeProfilePanel() {
+  document.getElementById('profile-overlay').classList.remove('open');
+  document.body.style.overflow = '';
+}
+
+function closeProfile(e) {
+  if (e.target === document.getElementById('profile-overlay')) closeProfilePanel();
+}
+
+function showPage(name) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.querySelectorAll('.nav-links a').forEach(a => a.classList.remove('active'));
+  document.getElementById('page-' + name).classList.add('active');
+  document.getElementById('nav-' + name).classList.add('active');
+  window.scrollTo(0, 0);
+  closeNav();
+}
+
+function toggleNav() {
+  document.getElementById('nav-links').classList.toggle('open');
+  document.getElementById('hamburger').classList.toggle('open');
+}
+
+function closeNav() {
+  document.getElementById('nav-links').classList.remove('open');
+  document.getElementById('hamburger').classList.remove('open');
+}
+
+initGrid();
+renderAwards();
